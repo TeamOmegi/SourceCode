8553df8e2d6e865a67411a76ef62cac13d15ea19
손민기 <thsalsrl7531@naver.com>
2024-04-29 13:10:39 +0900

    :sparkles: Feat-fe: Note Editor Toolbar 적용

---
+++ frontend/src/components/Editor/Toolbar.tsx
@@ -0,0 +1,35 @@
+import { Editor } from '@tiptap/react';
+import { Icon } from './Icons';
+
+interface ToolBarProps {
+  editor: Editor | null;
+}
+
+function ToolBar({ editor }: ToolBarProps) {
+  if (!editor) return null;
+
+  return (
+    <div className="flex items-center justify-center p-6 py-3 border-b-2 sm:gap-5">
+      <div className="flex items-center justify-center gap-2">
+        <Icon.H1 editor={editor} />
+        <Icon.H2 editor={editor} />
+        <Icon.H3 editor={editor} />
+        <Icon.H4 editor={editor} />
+      </div>
+      <div className="flex items-center justify-center gap-2">
+        <Icon.Bold editor={editor} />
+        <Icon.Italic editor={editor} />
+        <Icon.Strikethrough editor={editor} />
+      </div>
+
+      <div className="flex items-center justify-center gap-2">
+        <Icon.Code editor={editor} />
+        <Icon.CodeBlock editor={editor} />
+        <Icon.Quote editor={editor} />
+        {/* <Icon.AddPhoto editor={editor} /> */}
+      </div>
+    </div>
+  );
+}
+
+export default ToolBar;
