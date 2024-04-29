d23b221ca6ec4ccbef2d188b0ded2b20c9932a00
Jehun Yoo <percyu98@gmail.com>
2024-04-29 13:42:48 +0900

    :wrench: chore: disable spring security for development

---
+++ backend/core/src/main/java/io/omegi/core/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package io.omegi.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+			.build();
+	}
+}
