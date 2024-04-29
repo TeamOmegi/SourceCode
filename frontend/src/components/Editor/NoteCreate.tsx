e1ef42f8acab37dcee4531061f497775b732b51e
손민기 <thsalsrl7531@naver.com>
2024-04-29 13:09:11 +0900

    :sparkles: Feat-fe: Note Editor 생성

--- frontend/src/components/Editor/NoteCreate.tsx
+++ frontend/src/components/Editor/NoteCreate.tsx
@@ -1,6 +1,51 @@
+import { useEffect } from 'react';
+import { useEditor, EditorContent } from '@tiptap/react';
+import Toolbar from './Toolbar';
 
-const NoteCreate = () => {
+//tiptap
+import StarterKit from '@tiptap/starter-kit';
+import Highlight from '@tiptap/extension-highlight';
+import Placeholder from '@tiptap/extension-placeholder';
+import Image from '@tiptap/extension-image';
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
 
+import { common, createLowlight } from 'lowlight';
+
+interface NoteProprs {
+    content: string
+}
+
+const NoteCreate = ({ content }: NoteProprs) => {
+    const lowlight = createLowlight(common);
+    
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+            Highlight,
+            Image.configure({ inline: true, allowBase64: true }),
+            CodeBlockLowlight.configure({
+                lowlight,
+            }),
+        ],
+        content: content
+    });
+
+    useEffect(() => {
+        if (content) {
+        editor?.commands.setContent(content);
+        }
+    }, [content]);
+
+    return (
+        <div>
+            <div className="border-2 border-red-300">
+                <Toolbar editor={editor} />
+                <EditorContent
+                    editor={editor}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default NoteCreate;
