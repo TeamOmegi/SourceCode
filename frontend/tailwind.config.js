d7b47db7154c6618ffecdc2fedff2c777b8a6044
손민기 <thsalsrl7531@naver.com>
2024-04-30 14:50:16 +0900

    :wrench: Chore-fe: 기본 bg 스타일 설정 및 적용

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,42 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'primary': { //퍼플
-          '50': '#f2f0fc',
-          '100': '#e5e2f8',
-          '200': '#d8d4f5',
-          '300': '#cac6f1',
-          '400': '#bdb8ee',
-          '500': '#afaaea',
-          '600': '#908bbf',
-          '700': '#716e95',
-          '800': '#54526e',
-          '900': '#393749',
-          '950': '#201f27',
+        primary: {
+          //퍼플
+          50: "#f2f0fc",
+          100: "#e5e2f8",
+          200: "#d8d4f5",
+          300: "#cac6f1",
+          400: "#bdb8ee",
+          500: "#afaaea",
+          600: "#908bbf",
+          700: "#716e95",
+          800: "#54526e",
+          900: "#393749",
+          950: "#201f27",
         },
-      
-      'secondary': {//블루
-          '50': '#edf4fc',
-          '100': '#dbe8f9',
-          '200': '#c8ddf6',
-          '300': '#b5d2f3',
-          '400': '#a1c7ef',
-          '500': '#8cbcec',
-          '600': '#749ac0',
-          '700': '#5c7997',
-          '800': '#465a6f',
-          '900': '#303c49',
-          '950': '#1c2127',
+
+        secondary: {
+          //블루
+          50: "#edf4fc",
+          100: "#dbe8f9",
+          200: "#c8ddf6",
+          300: "#b5d2f3",
+          400: "#a1c7ef",
+          500: "#8cbcec",
+          600: "#749ac0",
+          700: "#5c7997",
+          800: "#465a6f",
+          900: "#303c49",
+          950: "#1c2127",
         },
-        'base' : '#171821'
+        main: {
+          100: "#171821",
+          200: "#F5F5F5",
+        },
       },
-      fontFamily:{
-        'sans': ['Pretendard-Regular', 'sans-serif'],
+      fontFamily: {
+        sans: ["Pretendard-Regular", "sans-serif"],
       },
     },
   },
@@ -56,13 +59,12 @@ module.exports = {
             borderRadius: "20px",
             backgroundClip: "padding-box",
             border: "6px solid transparent",
-            height: "20%"
-          }
-        }
-      }
+            height: "20%",
+          },
+        },
+      };
 
-      addUtilities(newUtilities, ["responsive", "hover"])
-    }
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
   ],
-}
-
+};
