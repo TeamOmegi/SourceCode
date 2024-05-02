e7ae594073756b46120945191d14aa5073de8eb5
손민기 <thsalsrl7531@naver.com>
2024-05-02 21:33:18 +0900

    :sparkles: Feat-fe: Tag 중복방지 기능

--- frontend/src/components/Editor/Tag.tsx
+++ frontend/src/components/Editor/Tag.tsx
@@ -2,25 +2,40 @@ import { useRef, useState } from "react";
 
 const Tag = () => {
   const tagRef = useRef<HTMLDivElement | null>(null);
-  const [hashTag, setHashTag] = useState<string>("");
+  const [tagName, setTagName] = useState<string>("");
   const [tagLen, setTagLen] = useState<number>(0);
 
   const handleTagChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setHashTag(e.target.value);
+    setTagName(e.target.value);
   };
 
   const handleOnKeyUp = (e: React.KeyboardEvent) => {
-    if (e.key == "Enter" && hashTag) {
+    if (e.key == "Enter" && tagName) {
       if (tagLen > 2) {
-        setHashTag("");
+        setTagName("");
         return;
+      } else if (tagLen > 0) {
+        const allTag = Array.from(
+          tagRef.current?.querySelectorAll("div") || [],
+        );
+
+        const isDuplication = allTag.find((tagDiv) => {
+          if (tagDiv.textContent === `# ${tagName}`) {
+            return tagDiv;
+          }
+        });
+
+        if (isDuplication != undefined) {
+          setTagName("");
+          return;
+        }
       }
 
       const inputTag = tagRef.current?.querySelector("input");
       const newTag = document.createElement("div");
       const className =
         "mr-3 font-light text-orange-500 bg-orange-50 rounded-3xl py-2 px-4 hover:cursor-pointer";
-      newTag.textContent = `# ${hashTag}`;
+      newTag.textContent = `# ${tagName}`;
       newTag.className = className;
       newTag.addEventListener("click", () => {
         handleTagDelete(newTag);
@@ -28,15 +43,15 @@ const Tag = () => {
 
       if (inputTag) {
         tagRef.current?.insertBefore(newTag, inputTag);
-        setHashTag("");
+        setTagName("");
         setTagLen(tagLen + 1);
       }
-    } else if (e.key == "Backspace" && hashTag == "") {
+    } else if (e.key == "Backspace" && tagName == "") {
       // div 제거하는 코드
       const lastTag = tagRef.current?.querySelector("input")?.previousSibling;
       if (lastTag) {
         tagRef.current?.removeChild(lastTag);
-        setHashTag("");
+        setTagName("");
         if (tagLen > 0) setTagLen(tagLen - 1);
       }
     }
@@ -45,7 +60,7 @@ const Tag = () => {
   const handleTagDelete = (tagDiv: HTMLDivElement) => {
     tagRef.current?.removeChild(tagDiv);
     const len = tagRef.current?.querySelectorAll("div").length || 0;
-    setHashTag("");
+    setTagName("");
     setTagLen(len);
   };
 
@@ -57,7 +72,7 @@ const Tag = () => {
       >
         <input
           type="text"
-          value={hashTag}
+          value={tagName}
           className={` bg-main-200 ml-2 h-7 w-48 text-xl tracking-[0.07em]  focus:outline-none ${tagLen == 3 ? "placeholder-rose-500" : "placeholder-[#868E96]"}`}
           placeholder={`해시태그 입력. (${tagLen}/3)`}
           onChange={handleTagChange}
