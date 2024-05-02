0a912f28ae98e25da1ff6faaa68f5b076420d5f3
고해림 <sunny9612@naver.com>
2024-05-02 09:11:43 +0900

    :sparkles: Feature-fe: MyNote Graph 구현

--- frontend/src/pages/MyNotePage.tsx
+++ frontend/src/pages/MyNotePage.tsx
@@ -1,8 +1,13 @@
+import NoteGraph from "../components/MyNote/NoteGraph/NoteGraph";
+
 const MyNotePage = () => {
   return (
     <div className="bg-default">
-      <div className="flex h-full w-full items-center justify-center text-5xl text-black">
-        <div>MyNotePage</div>
+      <div className="flex h-1/6 w-full bg-blue-300 text-5xl">내 노트</div>
+      <div className="flex h-5/6 w-full items-center justify-center rounded-xl">
+        <div className="flex h-5/6 w-11/12 items-center justify-center bg-gray-500 text-black">
+          <NoteGraph />
+        </div>
       </div>
     </div>
   );
