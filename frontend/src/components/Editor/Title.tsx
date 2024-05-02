3601dac65adea36df840a7d04588102b69a615ae
손민기 <thsalsrl7531@naver.com>
2024-05-02 20:11:09 +0900

    :sparkles: Feat-fe: Note Title 컴포넌트 생성

---
+++ frontend/src/components/Editor/Title.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const Title = () => {
+  const [title, setTitle] = useState<string>("");
+
+  useEffect(() => {
+    console.log("리랜더링");
+  }, []);
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+    console.log(title);
+  };
+
+  return (
+    <div className="flex w-full px-10 py-3">
+      <input
+        type="text"
+        className=" bg-main-200 h-20 w-full text-4xl font-black tracking-[0.07em] placeholder-[#868E96] focus:outline-none"
+        placeholder="제목을 입력하세요."
+        onChange={(e) => handleTitleChange(e)}
+      />
+    </div>
+  );
+};
+
+export default Title;
