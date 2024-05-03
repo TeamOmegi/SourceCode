a2e101b64a2f80ec602af06bc5e21c28ed41e53b
손민기 <thsalsrl7531@naver.com>
2024-05-03 16:14:25 +0900

    :sparkles: Feat-fe: Comfirm Hook 구현 및 적용

--- frontend/src/components/Editor/NoteCreate.tsx
+++ frontend/src/components/Editor/NoteCreate.tsx
@@ -12,6 +12,7 @@ import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 
 import { common, createLowlight } from "lowlight";
 import Title from "./Title";
+import { useQuestion, useWarnning } from "../../hooks/useComfirm";
 
 interface NoteProprs {
   content: string;
@@ -45,15 +46,28 @@ const NoteCreate = ({ content }: NoteProprs) => {
     }
   }, [content]);
 
-  useEffect(() => {
-    if (submitContent) {
-      console.log("적용완료");
-      editor?.commands.setContent(submitContent);
-    }
-  }, [submitContent]);
+  // useEffect(() => {
+  //   if (submitContent) {
+  //     console.log("적용완료");
+  //     editor?.commands.setContent(submitContent);
+  //   }
+  // }, [submitContent]);
 
   const handleNoteCreate = () => {
+    useQuestion({
+      title: "Note Create",
+      fireText: "Note를 생성하시겠습니까?",
+      resultText: "Note가 생성되었습니다.",
+    });
     setSubmitContent(editor?.getHTML());
+  };
+
+  const handleNoteDelete = () => {
+    useWarnning({
+      title: "Note Initialization",
+      fireText: "Note를 초기화하시겠습니까?",
+      resultText: "Note가 초기화되었습니다.",
+    });
   };
 
   return (
@@ -70,7 +84,7 @@ const NoteCreate = ({ content }: NoteProprs) => {
         <div className="flex h-12 w-24 items-center justify-center">
           <div
             className="bg-main-200 hover:text-main-200 flex h-10 w-20 select-none items-center justify-center rounded-2xl border-[2px] border-[#F2527D] text-sm font-extrabold text-[#868E96] shadow-[0_15px_35px_rgba(0,0,0,0.2)] hover:h-12 hover:w-24 hover:cursor-pointer hover:bg-[#F2527D] hover:text-base hover:duration-200"
-            onClick={handleNoteCreate}
+            onClick={handleNoteDelete}
           >
             <div>초기화</div>
           </div>
