a2e31aa0709d52602d43555db2e2528b3bc8308d
고해림 <sunny9612@naver.com>
2024-05-07 17:30:09 +0900

    :sparkles: Feature-fe: Dashboard axios 생성

---
+++ frontend/src/api/dashboardAxios.tsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+
+const BASE_URL = "";
+
+// ErrorSummary
+interface ErrorSummary {
+  solvedErrorCount: number;
+  unsolvedErrorCount: number;
+  recentlyErrorOccurredAt: string;
+}
+
+// 에러 한눈에 보기
+export const getErrorSummary = async (): Promise<ErrorSummary | undefined> => {
+  try {
+    const response = await axios.get(`${BASE_URL}/error/summary`);
+    return response.data;
+  } catch (error) {
+    console.error(error, "Fail getErrorSummary");
+  }
+};
+
+// NoteSummary
+interface NoteSummary {
+  noteCount: number;
+  linkCount: number;
+  backlinkCount: number;
+}
+
+// 노트 한눈에 보기
+export const getNoteSummary = async (): Promise<NoteSummary | undefined> => {
+  try {
+    const response = await axios.get(`${BASE_URL}/note/summary`);
+    return response.data;
+  } catch (error) {
+    console.error(error, "Fail getNoteSummary");
+  }
+};
+
+// ErrorList
+interface ErrorItem {
+  errorId: number;
+  projectId: number;
+  serviceId: number;
+  error: string;
+}
+
+interface RecentErrorListResponse {
+  status: number;
+  message: string;
+  result: ErrorItem[];
+}
+
+// 에러 최근 목록
+export const getRecentErrorList = async (): Promise<
+  ErrorItem[] | undefined
+> => {
+  try {
+    const response =
+      await axios.get<RecentErrorListResponse>("/api/recentErrors");
+    return response.data.result;
+  } catch (error) {
+    // 오류 처리
+    console.error("Error occurred while fetching recent error list:", error);
+  }
+};
