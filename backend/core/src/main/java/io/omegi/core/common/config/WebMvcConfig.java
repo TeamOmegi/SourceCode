4ab9c0c16704b02e3cedc00f57b7be94946e59e4
Jehun Yoo <percyu98@gmail.com>
2024-05-08 17:11:31 +0900

    :bug: Fix-backend: CORS

--- backend/core/src/main/java/io/omegi/core/common/config/WebMvcConfig.java
+++ backend/core/src/main/java/io/omegi/core/common/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.omegi.core.common.presentation.support.LoginArgumentResolver;
@@ -15,5 +16,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(new LoginArgumentResolver());
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("/**");
 	}
 }
