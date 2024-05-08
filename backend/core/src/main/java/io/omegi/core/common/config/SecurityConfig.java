c8aa3f848355e4fd1dc3d6854db7d8558de2aec9
Jehun Yoo <percyu98@gmail.com>
2024-05-08 16:23:14 +0900

    :bug: Fix-backend: CORS

--- backend/core/src/main/java/io/omegi/core/common/config/SecurityConfig.java
+++ backend/core/src/main/java/io/omegi/core/common/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package io.omegi.core.common.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -15,7 +20,25 @@ public class SecurityConfig {
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.csrf(AbstractHttpConfigurer::disable)
+			.cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
 			.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
 			.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedOriginPatterns(List.of("*"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setExposedHeaders(List.of("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+
 	}
 }
