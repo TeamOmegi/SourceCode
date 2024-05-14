35e2b4008de24622649325a73cc22ad91841908e
고해림 <sunny9612@naver.com>
2024-05-14 17:35:00 +0900

    :lipstick: Design : AllNoteListPage CSS

--- frontend/src/components/AllNote/AllNoteContainer.tsx
+++ frontend/src/components/AllNote/AllNoteContainer.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useContentParser } from "../../hooks/useContentParser";
+import { useDanger } from "../../hooks/useComfirm";
+import { noteDelete } from "../../api/myNoteAxios";
 
 interface AllNote {
   noteId: number;
@@ -32,56 +34,79 @@ const AllNoteContainer = ({ notes }: Props) => {
     setAllNotes(notes);
   }, [notes]);
 
-  // 노트를 클릭했을 때 상세 페이지로 이동하는 함수
+  const handleNoteDelete = async (e: React.MouseEvent, noteId: number) => {
+    e.stopPropagation();
+    const result = await useDanger({
+      title: "노트를 삭제하시겠습니까?",
+      fireText: "영구적으로 삭제됩니다.",
+    });
+
+    if (result) noteDelete(noteId);
+  };
+
   const handleNoteClick = (note: AllNote) => {
     if (note.isMine) {
-      navigate(`/omegi/myNote/${note.noteId}`); // MyNoteDetail 페이지로 이동
+      navigate(`/omegi/myNote/${note.noteId}`);
     } else {
-      navigate(`/omegi/allNote/${note.noteId}`); // AllNoteDetail 페이지로 이동
+      navigate(`/omegi/allNote/${note.noteId}`);
     }
   };
 
   return (
-    <div className="mt-5 flex h-full w-full flex-shrink-0 flex-col overflow-y-scroll scrollbar-webkit">
-      {allNotes.map((note) => (
-        <div
-          key={note.noteId}
-          className="mb-5 ml-5 mr-5 box-border flex items-center justify-between rounded-xl border-[1px] border-gray-300 bg-white py-3 pb-2 pl-3 shadow-lg"
-          onClick={() => handleNoteClick(note)}
-          style={{ cursor: "pointer" }}
-        >
-          <div className="box-border flex h-auto w-full flex-col justify-start">
-            <div className="box-border flex  flex-col">
-              <h3 className="p-1 text-lg font-semibold">{note.title}</h3>
-              <p className="mr-5 box-border line-clamp-2 text-ellipsis whitespace-normal px-2 text-sm">
-                {note.content}
-              </p>
-            </div>
-            <div className="mb-1 ml-2 mt-2 flex justify-between">
-              <div className="flex items-center">
+    <div className="mx-3 mt-5 flex h-full w-full flex-shrink-0 flex-col overflow-y-scroll scrollbar-webkit">
+      <div className="flex flex-wrap justify-between">
+        {allNotes.map((note) => (
+          <div
+            key={note.noteId}
+            className="mb-3 mr-3 box-border flex w-[530px] items-center justify-between rounded-xl border-[1px] border-gray-300 bg-white py-3 pb-2 pl-3 shadow-lg"
+            onClick={() => handleNoteClick(note)}
+            style={{ cursor: "pointer" }}
+          >
+            <div className="box-border flex h-full w-full justify-start">
+              <div>
                 <img
-                  src="../public/icons/ProfileDefault.png"
-                  alt="프로필 이미지"
-                  className="h-4 w-4 rounded-full"
+                  src="/mingi.jpg"
+                  alt="민기 이미지"
+                  className="h-28 w-28 rounded-lg"
                 />
-                <p className="text-xs text-gray-500">{note.user.username}</p>
               </div>
-              <div className="mb-1 mr-5 flex">
-                <p className="text-xs text-gray-500">
-                  {note.createdAt.split("T")[0]}
-                </p>
-                {note.isMine && (
-                  <img
-                    src="/icons/DeleteIcon1.png"
-                    alt="삭제 아이콘"
-                    className="mx-3 h-4 w-4"
-                  />
-                )}
+              <div className="ml-5 box-border flex w-full flex-col justify-between">
+                <div className="box-border flex flex-col">
+                  <h3 className="p-1 text-lg font-semibold">{note.title}</h3>
+                  <p className="box-border line-clamp-2 text-ellipsis whitespace-normal px-2 text-sm">
+                    {note.content}
+                  </p>
+                </div>
+                <div className="mb-1 flex items-end justify-between">
+                  <div className="ml-1 flex items-center">
+                    <img
+                      src="../public/icons/ProfileDefault.png"
+                      alt="프로필 이미지"
+                      className="mr-1 h-4 w-4 rounded-full"
+                    />
+                    <p className="text-xs text-gray-500">
+                      {note.user.username}
+                    </p>
+                  </div>
+                  <div className="mr-4 flex items-center">
+                    <p className="mr-3 text-xs text-gray-500">
+                      {note.createdAt.split("T")[0]}
+                    </p>
+                    {note.isMine && (
+                      <img
+                        src="/icons/DeleteIcon1.png"
+                        alt="삭제 아이콘"
+                        className="h-4 w-4"
+                        onClick={(e) => handleNoteDelete(e, note.noteId)}
+                      />
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/components/AllNote/AllNoteList.tsx
+++ frontend/src/components/AllNote/AllNoteList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getAllNoteList } from "../../api/allNoteAxios";
 import AllNoteContainer from "./AllNoteContainer";
 
@@ -19,34 +19,32 @@ const AllNoteList = () => {
   const [allNoteList, setAllNoteList] = useState<AllNote[]>([]);
   const [searchKeyword, setSearchKeyword] = useState<string>("");
 
-  const getAllNotes = async () => {
-    try {
-      const allNotesResponse = await getAllNoteList(searchKeyword);
-      console.log("히히 오줌발싸", allNotesResponse.notes);
-
-      setAllNoteList([...allNotesResponse.notes]);
-    } catch (error) {
-      console.error("Fail getAllNotes ", error);
-    }
-  };
-
   useEffect(() => {
+    const getAllNotes = async () => {
+      const allNotes = await getAllNoteList("");
+      console.log(allNotes.notes, "????????????????????");
+      setAllNoteList([...allNotes.notes]);
+    };
     getAllNotes();
   }, []);
 
-  const handleSearch = () => {
-    getAllNotes();
+  const handleSearch = async () => {
+    const allNoteData = await getAllNoteList(searchKeyword);
+    setAllNoteList([...allNoteData.notes]);
   };
 
   return (
-    <div className="box-border flex h-full w-full flex-col items-center justify-center rounded-xl p-5 text-xl text-black">
-      <div className="flex h-[10%] w-full items-center justify-end">
-        <div className=" mr-10 mt-2 flex h-10 w-1/3 rounded-2xl border-[1px] border-gray-400 bg-white pl-2 text-sm focus-within:border-secondary-400 focus-within:ring focus-within:ring-secondary-200">
+    <div className="box-border flex h-full w-full flex-col items-center justify-center rounded-xl p-7 text-xl text-black">
+      <div className="flex h-[10%] w-full items-center justify-start">
+        <div className="ml-5 mt-2 flex h-10 w-1/3 rounded-2xl border-[1px] border-gray-400 bg-white pl-2 text-sm focus-within:border-secondary-400 focus-within:ring focus-within:ring-secondary-200">
           <input
             type="text"
             placeholder="여기에 검색해보세요!"
             value={searchKeyword}
             onChange={(e) => setSearchKeyword(e.target.value)}
+            onKeyUp={(e) => {
+              if (e.key == "Enter") handleSearch();
+            }}
             className="ml-2 h-full w-full bg-transparent text-sm text-gray-700 outline-none"
           />
           <img
@@ -57,7 +55,7 @@ const AllNoteList = () => {
           />
         </div>
       </div>
-      <div className="flex h-[90%] w-full">
+      <div className="flex h-[90%] w-full items-center justify-center">
         <AllNoteContainer notes={allNoteList} />
       </div>
     </div>
