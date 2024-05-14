ff1ef845708bd1148b66bf3a4201842ae8d06496
Jehun Yoo <percyu98@gmail.com>
2024-05-14 14:16:35 +0900

    :sparkles: Feat-be: add TraceAspect

--- backend/core/src/main/java/io/omegi/core/common/config/AspectConfig.java
+++ backend/core/src/main/java/io/omegi/core/common/config/AspectConfig.java
@@ -3,6 +3,13 @@ package io.omegi.core.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.omegi.core.common.aspect.TraceAspect;
+
 @Configuration
 public class AspectConfig {
+
+	@Bean
+	public TraceAspect traceAspect() {
+		return new TraceAspect();
+	}
 }
