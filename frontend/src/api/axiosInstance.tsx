4d4ecbdd5be0a414fe44314a482ff7b2b88adb6f
손민기 <thsalsrl7531@naver.com>
2024-05-16 14:47:07 +0900

    :wrench: Chore-fe: axios 수정

--- frontend/src/api/axiosInstance.tsx
+++ frontend/src/api/axiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-const baseURL = "http://core:8081";
-// const baseURL = "http://k10a308.p.ssafy.io:8081";
+//const baseURL = "http://core:8081";
+const baseURL = "http://k10a308.p.ssafy.io:8081";
 const axiosInstance = axios.create({
   baseURL: baseURL,
   withCredentials: true,
