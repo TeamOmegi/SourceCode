cfd36dff500597bf1f795aa3aa01e9af548022e8
손민기 <thsalsrl7531@naver.com>
2024-05-16 13:24:11 +0900

    :wrench: Chore-fe: gGITHUB_LOGIN_URL 변경

--- frontend/src/pages/IndexPage.tsx
+++ frontend/src/pages/IndexPage.tsx
@@ -2,8 +2,15 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
-const GITHUB_LOGIN_URL =
+const GITHUB_LOGIN_URL1 =
+  "http://k10a308.p.ssafy.io:8081/oauth2/authorization/github";
+const GITHUB_LOGIN_URL2 =
+  "http://k10a308.p.ssafy.io:8081/oauth2/authorization/github";
+const GITHUB_LOGIN_URL3 =
   "http://k10a308.p.ssafy.io/oauth2/authorization/github";
+const GITHUB_LOGIN_URL4 =
+  "http://k10a308.p.ssafy.io/oauth2/authorization/github";
+
 const IndexPage = () => {
   const navigate = useNavigate();
   useEffect(() => {
@@ -18,8 +25,18 @@ const IndexPage = () => {
     checkLogin();
   }, [navigate]);
 
-  const handleLoginClick = () => {
-    window.location.href = GITHUB_LOGIN_URL;
+  const handleLoginClick1 = () => {
+    window.location.href = GITHUB_LOGIN_URL1;
+  };
+
+  const handleLoginClick2 = () => {
+    window.location.href = GITHUB_LOGIN_URL2;
+  };
+  const handleLoginClick3 = () => {
+    window.location.href = GITHUB_LOGIN_URL3;
+  };
+  const handleLoginClick4 = () => {
+    window.location.href = GITHUB_LOGIN_URL4;
   };
 
   return (
@@ -27,10 +44,28 @@ const IndexPage = () => {
       메인화면으로 가고싶다면 경로에 /omegi를 추가로 입력해라
       <div className="h-5/6">
         <button
-          onClick={handleLoginClick}
+          onClick={handleLoginClick1}
           className="box-border rounded-2xl border-sky-200 bg-sky-200 px-5 py-2 text-xl"
         >
-          로그인
+          로그인1
+        </button>
+        <button
+          onClick={handleLoginClick2}
+          className="box-border rounded-2xl border-sky-200 bg-sky-200 px-5 py-2 text-xl"
+        >
+          로그인2
+        </button>
+        <button
+          onClick={handleLoginClick3}
+          className="box-border rounded-2xl border-sky-200 bg-sky-200 px-5 py-2 text-xl"
+        >
+          로그인3
+        </button>
+        <button
+          onClick={handleLoginClick4}
+          className="box-border rounded-2xl border-sky-200 bg-sky-200 px-5 py-2 text-xl"
+        >
+          로그인4
         </button>
       </div>
     </div>
