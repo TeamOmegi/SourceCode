3e4140e30e0a263ea80511833b86c59a19e929b4
손민기 <thsalsrl7531@naver.com>
2024-05-19 19:10:23 +0900

    :wrench: Chore-fe: Blur 로직변경

--- frontend/src/components/Dashboard/ErrorGraph.tsx
+++ frontend/src/components/Dashboard/ErrorGraph.tsx
@@ -36,6 +36,9 @@ const ErrorGraph = ({ projectId }: Props) => {
     if (nodes.length != 0 && links.length != 0) {
       draw();
       errorCheck();
+    } else {
+      const svg = d3.select(svgRef.current);
+      svg.selectAll("*").remove();
     }
   }, [nodes, errorMap]);
 
@@ -47,14 +50,14 @@ const ErrorGraph = ({ projectId }: Props) => {
 
     errorIdxArr.map((idx) => {
       if (idx == -1) return;
-      d3.select(pjtRef?.current[idx]).style("filter", "url(#error-shadow)");
+      d3.select(pjtRef?.current[idx]).attr("fill", "#ff5050");
     });
   };
 
   const draw = () => {
     const width = 660;
     const height = 240;
-    const radius = 20;
+    const radius = 25;
     const margin = 100;
 
     const svg = d3
@@ -67,7 +70,7 @@ const ErrorGraph = ({ projectId }: Props) => {
       .append("clipPath")
       .attr("id", "circleClip")
       .append("circle")
-      .attr("r", radius + 4);
+      .attr("r", radius);
 
     //svg에 정의하는 코드 (svg 내에서 사용가능한 기능 만들어놓는다 생각하면됨)
     var defs = svg.append("defs");
@@ -96,21 +99,29 @@ const ErrorGraph = ({ projectId }: Props) => {
       .attr("stroke-width", 1)
       .attr("marker-end", "url(#arrowhead)"); //정의된거 사용하는 방법
 
-    //filter(blur같은)를 정의함
-    var filter = defs
+    // 블러 필터
+    var blurFilter = defs.append("filter").attr("id", "blur-filter");
+
+    blurFilter
+      .append("feGaussianBlur")
+      .attr("in", "SourceGraphic")
+      .attr("stdDeviation", 5);
+
+    //쉐도우 필터
+    var shadowFilter = defs
       .append("filter")
       .attr("id", "default-shadow")
       .attr("height", "130%");
 
     // 블러 생성
-    filter
+    shadowFilter
       .append("feGaussianBlur")
       .attr("in", "SourceAlpha") //그림자 (흑,백)
-      .attr("stdDeviation", 5)
+      .attr("stdDeviation", 25)
       .attr("result", "defaultBlur");
 
     // 흑백인 그림자에 색상넣기
-    filter
+    shadowFilter
       .append("feColorMatrix")
       .attr("in", "defaultBlur")
       .attr("type", "matrix")
@@ -118,50 +129,15 @@ const ErrorGraph = ({ projectId }: Props) => {
       .attr("result", "defaultBlurColor");
 
     //필터에 feOffset을 적용 (필터가 적용될 위치) .attr("dx" or "dy" 설정가능)
-    filter
+    shadowFilter
       .append("feOffset")
       .attr("in", "defaultBlurColor")
       .attr("result", "offsetBlur");
 
-    var feMerge = filter.append("feMerge");
+    var feMerge = shadowFilter.append("feMerge");
 
     feMerge.append("feMergeNode").attr("in", "defaultOffsetBlur");
     feMerge.append("feMergeNode").attr("in", "SourceGraphic");
-
-    // 에러블러 정의하기
-    var errorfilter = defs
-      .append("filter")
-      .attr("id", "error-shadow")
-      .attr("height", "130%");
-
-    // 블러 생성
-    errorfilter
-      .append("feGaussianBlur")
-      .attr("in", "SourceAlpha")
-      .attr("stdDeviation", 10)
-      .attr("result", "errorBlur");
-
-    // 다른 색상의 색상 변환
-    errorfilter
-      .append("feColorMatrix")
-      .attr("in", "errorBlur")
-      .attr("type", "matrix")
-      .attr("values", "1 1 1 1 1  1 0 0 0 0  0 0 1 0 0  0 0 0 1 0") // 색상 변환 값
-      .attr("result", "errorBlurColor");
-
-    // 필터에 feOffset을 적용 (필터가 적용될 위치)
-    errorfilter
-      .append("feOffset")
-      .attr("in", "errorBlurColor")
-      .attr("result", "errorOffsetBlur");
-
-    // 필터들을 결합하는 feMerge 생성
-    var feMerge2 = errorfilter.append("feMerge");
-
-    // 필터에 연결할 노드 추가
-    feMerge2.append("feMergeNode").attr("in", "errorOffsetBlur");
-    feMerge2.append("feMergeNode").attr("in", "SourceGraphic");
-    /////
 
     //노드생성
     const node = svg
@@ -175,16 +151,24 @@ const ErrorGraph = ({ projectId }: Props) => {
 
         d3.select(this)
           .append("circle")
-          .attr("r", radius)
+          .attr("r", radius + 12)
           .attr("pjt", `${d.serviceId}`)
-          .attr("clip-path", "url(#circleClip)")
-          .attr("fill", "white")
-          .style("filter", "url(#default-shadow)");
+          .attr("fill", "#6699ff")
+          .attr("class", "blur-circle")
+          .style("filter", "url(#blur-filter)")
+          .style("opacity", 0.4);
+        //.style("filter", "url(#default-shadow)");
+
+        d3.select(this)
+          .append("circle")
+          .attr("r", radius + 1)
+          .attr("pjt", `${d.serviceId}`)
+          .attr("fill", "white");
 
         d3.select(this)
           .append("image")
           .attr("xlink:href", `${d.serviceImageUrl}`)
-          //.attr("clip-path", "url(#circleClip)") //이미지 오리기 (위에 정의해놓음)
+          .attr("clip-path", "url(#circleClip)") //이미지 오리기 (위에 정의해놓음)
           .attr("width", 2 * radius) // 이미지의 크기
           .attr("height", 2 * radius) // 이미지의 크기
           .attr("x", -radius)
@@ -193,20 +177,36 @@ const ErrorGraph = ({ projectId }: Props) => {
         d3.select(this)
           .append("text")
           .text((d: any) => d.serviceName)
-          .attr("x", -25)
-          .attr("y", -5);
+          .attr("x", -50)
+          .attr("y", +50)
+          .style("font-size", "12px") // 폰트 크기 설정
+          .style("font-weight", "bold");
       });
 
     const ticked = () => {
       link
-        .attr("x1", (d: any) => d.source.x)
-        .attr("y1", (d: any) => d.source.y)
+        .attr("x1", (d: any) => {
+          const dx = d.target.x - d.source.x;
+          const dy = d.target.y - d.source.y;
+          const length = Math.sqrt(dx * dx + dy * dy);
+          const offsetX = dx * (radius / (length == 0 ? 1 : length));
+          return d.source.x + offsetX;
+        })
+        .attr("y1", (d: any) => {
+          {
+            const dx = d.target.x - d.source.x;
+            const dy = d.target.y - d.source.y;
+            const length = Math.sqrt(dx * dx + dy * dy);
+            const offsetY = dy * (radius / (length == 0 ? 1 : length));
+            return d.source.y + offsetY;
+          }
+        })
         .attr("x2", (d: any) => {
           // 시작점이 노드의 중심에 연결되도록 조정
           const dx = d.target.x - d.source.x;
           const dy = d.target.y - d.source.y;
           const length = Math.sqrt(dx * dx + dy * dy);
-          const offsetX = (dy / (length == 0 ? 1 : length)) * radius;
+          const offsetX = dx * ((radius + 7) / (length == 0 ? 1 : length));
           return d.target.x - offsetX;
         })
         .attr("y2", (d: any) => {
@@ -214,7 +214,7 @@ const ErrorGraph = ({ projectId }: Props) => {
           const dx = d.target.x - d.source.x;
           const dy = d.target.y - d.source.y;
           const length = Math.sqrt(dx * dx + dy * dy);
-          const offsetY = (dy / (length == 0 ? 1 : length)) * radius;
+          const offsetY = dy * ((radius + 7) / (length == 0 ? 1 : length));
           return d.target.y - offsetY;
         });
       node.attr("transform", (d: any) => `translate(${d.x}, ${d.y})`);
@@ -238,7 +238,7 @@ const ErrorGraph = ({ projectId }: Props) => {
         d3.forceLink(links).id((d: any) => d.serviceId),
       )
       .force("charge", d3.forceManyBody().strength(-5000))
-      .force("center", d3.forceCenter(width / 2, height / 2))
+      .force("center", d3.forceCenter(width / 2, height / 2 + 30))
       .force(
         "x",
         d3.forceX().x((d) => Math.max(margin, Math.min(width - radius, d.x!))),
@@ -275,7 +275,7 @@ const ErrorGraph = ({ projectId }: Props) => {
         .on("end", dragended),
     );
 
-    const arr = svgRef?.current?.querySelectorAll("g circle");
+    const arr = svgRef?.current?.querySelectorAll("g circle.blur-circle");
     pjtRef.current = [...arr];
   };
 
